92f9f559e6a5baa95525843e18da672da072243d
Daniel Denton <highdynamics@gmail.com>
2020-09-17 09:30:50 -0500

    Updates

---
+++ precache-manifest.3a7be9cfc68b9da72e6e0118a4fd938d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad2f35951488884a62fcd96c18a83bd8",
+    "url": "/dice_roller/index.html"
+  },
+  {
+    "revision": "942d1ae8a3fffde6662f",
+    "url": "/dice_roller/static/css/main.ce106bb2.chunk.css"
+  },
+  {
+    "revision": "3c7aa07d3b6d6d5bdd23",
+    "url": "/dice_roller/static/js/2.de7b1510.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dice_roller/static/js/2.de7b1510.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "942d1ae8a3fffde6662f",
+    "url": "/dice_roller/static/js/main.66cfa741.chunk.js"
+  },
+  {
+    "revision": "fd92b75bee52c1ec657d",
+    "url": "/dice_roller/static/js/runtime-main.9335511d.js"
+  }
+]);
